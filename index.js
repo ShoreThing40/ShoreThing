23724904a10c95fcda2a63c883c205628e49188f
Alex Wolinsky <alexwolinsky1@gmail.com>
2021-02-08 16:25:59 -0800

    initial tailwind.css setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import App from './client/containers/App.jsx';
 // also add styles here when applicable
 // import styles from './styles.scss'
 
