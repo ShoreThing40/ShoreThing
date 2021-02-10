2df6adae8b9edd041adc367b3639e141a0403dad
Colin <cjvdgf@gmail.com>
2021-02-10 12:16:45 -0800

    update Oauth and frontend connection

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,20 +6,18 @@ const saltRounds = 10;
 module.exports = {
 
   createPassword: function (req, res, next) {
-    const { password, username } = req.body;
+    const { username, password, location} = req.body;
     console.log(req.body)
     bcrypt.hash(password, saltRounds, function(err, hash) {
-      if (err) { 
-        return next(err);
-      }
-      req.body = { hash, username }
+      if (err) return next(err);
+      req.body = { hash, username, location }
       return next();
     });
   },
 
   checkPassword: function (req, res, next) {
     // console.log('getting username')
-    const { password, username, location } = req.body;
+    const {username, password, location } = req.body;
     const text = `SELECT * FROM Users WHERE (username=$1)`;
     let data;
       db.query(text, [username])
@@ -28,7 +26,7 @@ module.exports = {
           data = user;
           bcrypt.compare(password, data.rows[0].user_pw, function(err, res) {
             if (err) return next(err);
-            res.locals.result = data;
+            res.locals.result = user;
             return next();
           });
         })
@@ -36,9 +34,9 @@ module.exports = {
   },
 
   storeUserInfo: function (req, res, next) {
-    const { username, hash } = req.body;
-    const text = `INSERT INTO Users (username, user_pw) VALUES ($1, $2)`;
-    db.query(text, [username, hash ])
+    const {username, hash, location } = req.body;
+    const text = `INSERT INTO Users (username, user_pw, home_zip) VALUES ($1, $2, $3)`;
+    db.query(text, [username, hash, location ])
     .then(() => next())
     .catch(err => next({ error: err }));
   }
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/create_pw', authController.createPassword, authController.storeUserInfo, (req, res) => res.status(200).json(res.locals.result));
+router.post('/signup', authController.createPassword, authController.storeUserInfo, (req, res) => res.status(200).json(res.locals.result));
 
 router.post('/check_pw', authController.checkPassword, (req, res) => res.status(200).json(res.locals.result));
 
