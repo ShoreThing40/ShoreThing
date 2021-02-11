8a82f04bf9265501819a85ccc8a8cd134f4ebe9e
Alex Wolinsky <alexwolinsky1@gmail.com>
2021-02-11 12:23:41 -0800

    additional styling on card containers
    Co-authored-by: Lex Choi <lexchoi3@gmail.com>

--- client/index.jsx
+++ client/index.jsx
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import App from './containers/App.jsx';
 // also add styles here when applicable
-import styles from './styles.css'
+import styles from './styles.scss'
 
 
 ReactDOM.render(
