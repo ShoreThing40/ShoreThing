874abdcfe0e669ceec755dab2e8ceeb00accd66e
lexiphur <lexchoi3@gmail.com>
2021-02-11 15:13:32 -0800

    implement refresh feature for favorite cards

--- client/components/FavoritedParks.jsx
+++ client/components/FavoritedParks.jsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import FavParkCard from './FavParkCard.jsx';
 // import DummyCard from './DummyCard.jsx';
 
 const FavoritedParks = () => {
   const [favBeaches, setFavBeaches] = useState([]);
-  
+
   useEffect(() => {
     // request to retrieve favorite beaches
     fetch(`/trails/interested/${sessionStorage.getItem('user_id')}`)
@@ -13,42 +13,60 @@ const FavoritedParks = () => {
         console.log('what is data', data)
         setFavBeaches(data)
       })
-      .catch((err) => {throw new Error(err)});
+      .catch((err) => { throw new Error(err) });
   }, []);
-  
+
   const favoriteBtnHandler = (id) => {
     console.log('remove button clicked', id);
     fetch(`/trails/interested/${sessionStorage.getItem('user_id')}/${id}`, {
       method: 'DELETE',
-      headers: {'Content-Type': 'application/json; charset=UTF-8'},
-      body: JSON.stringify({user_id: sessionStorage.getItem('user_id'), trail_id: id})
+      headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+      body: JSON.stringify({ user_id: sessionStorage.getItem('user_id'), trail_id: id })
     })
       .then(() => {
         // fetch table to reset state to rerender here
         fetch(`/trails/interested/${sessionStorage.getItem('user_id')}`)
-        .then((favorites) => favorites.json())
-        .then((data) => {
-          console.log('what is data', data)
-          setFavBeaches(data)
-        })
-        .catch((err) => {throw new Error(err)});
-      }) 
+          .then((favorites) => favorites.json())
+          .then((data) => {
+            console.log('what is data', data)
+            setFavBeaches(data)
+          })
+          .catch((err) => { throw new Error(err) });
+      })
   };
-  
+
+  const refreshHandler = () => {
+    // request to retrieve favorite beaches
+    fetch(`/trails/interested/${sessionStorage.getItem('user_id')}`)
+      .then((favorites) => favorites.json())
+      .then((data) => {
+        console.log('what is data', data)
+        setFavBeaches(data)
+      })
+      .catch((err) => { throw new Error(err) });
+  }
+
   const cardHolder = [];
-  for (let i = 0; i < favBeaches.length; i++){
-    cardHolder.push(<FavParkCard key={favBeaches[i].trail_id} parkId={favBeaches[i].trail_id} favoriteBtnHandler={favoriteBtnHandler}/>)
+  for (let i = 0; i < favBeaches.length; i++) {
+    cardHolder.push(<FavParkCard key={favBeaches[i].trail_id} parkId={favBeaches[i].trail_id} favoriteBtnHandler={favoriteBtnHandler} />)
   }
   if (!cardHolder.length) return null;
   return (
     <div className="card-set">
-    <h2 className="card-holder-label">Favorites</h2>
-    <div className="cardHolder">
-      <div className="scroll-div">
-      {cardHolder}
+      <div >
+        <div style={{ marginTop: '1rem' }}>
+          <button type="button" className="btn btn-info" onClick={() => refreshHandler()}>Refresh</button>
+        </div>
+        <div style={{ marginTop: '1rem', paddingTop: '4rem' }}>
+          <h2 className="card-holder-label">Favorites</h2>
+        </div>
+      </div>
+      <div className="cardHolder">
+        <div className="scroll-div">
+          {cardHolder}
+        </div>
       </div>
     </div>
-  </div>
   )
 };
 
